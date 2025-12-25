b5091a861e518543c15cb137c2e10fbbc0e51c71
mohsen <mohsenmansourikhah@gmail.com>
2025-12-25 19:52:38 +0330

    feat: add api viewsets with permissions

--- api_module/views.py
+++ api_module/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from .models import Product, Service, Reservation, Gallery
 from . serializer import ReservationSerializer, ServiceSerializer, GallerySerializer, ProductSerializer
 from rest_framework.viewsets import ModelViewSet
@@ -6,16 +7,21 @@ from rest_framework.viewsets import ModelViewSet
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class ServiceViewSet(ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class GalleryViewSet(ModelViewSet):
     queryset = Gallery.objects.all()
     serializer_class = GallerySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class ReservationViewSet(ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    permission_classes = [AllowAny]
+    http_method_names = ["post", "get"]
